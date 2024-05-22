98f484618a4d6faa7147b59ee6d57a30cb574449
John Doherty <johndoherty1984@live.com>
2024-05-22 12:54:49 +0000

    Update PostEvent model with str method and orangise by date

--- events_listing/models.py
+++ events_listing/models.py
@@ -30,6 +30,12 @@ class PostEvent(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
 
+    class Meta:
+        ordering = ["date"]
+
+    def __str__(self):
+        return f"{self.event_name} | Posted by {self.author} | Event date {self.date.strftime('%d %B %Y')}"
+
 class Comment(models.Model):
     post = models.ForeignKey(
         PostEvent, on_delete=models.CASCADE, related_name="comments")
