96878864227db94fdafcf098b561cce4c2b84e16
John Doherty <johndoherty1984@live.com>
2024-05-22 11:18:40 +0000

    Change debug status

--- myproject/settings.py
+++ myproject/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rbhhk88+3l+w%2q0+uxe62zyq_-67w=bmmn-xb*yn)!o_7%6m4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-jdohertydev-runit-jd1464g5zvp.ws-eu114.gitpod.io','.herokuapp.com']
 
