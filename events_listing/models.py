84975c10075a1d6e832c725af9b15d87a054295b
John Doherty <johndoherty1984@live.com>
2024-05-22 12:38:57 +0000

    Add comment model

--- events_listing/models.py
+++ events_listing/models.py
@@ -29,3 +29,12 @@ class PostEvent(models.Model):
     course_map = models.URLField(blank=True, null=True)
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        PostEvent, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)    
--- myproject/settings.py
+++ myproject/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rbhhk88+3l+w%2q0+uxe62zyq_-67w=bmmn-xb*yn)!o_7%6m4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-jdohertydev-runit-jd1464g5zvp.ws-eu114.gitpod.io','.herokuapp.com']
 
@@ -124,7 +124,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
