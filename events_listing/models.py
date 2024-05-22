1cb7923e3eb32595edb36f62615e48526af39953
John Doherty <johndoherty1984@live.com>
2024-05-22 11:18:01 +0000

    Add event post model, add trused origins

--- events_listing/models.py
+++ events_listing/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
+
+class PostEvent(models.Model):
+    event_name = models.CharField(max_length=200)
+    date = models.DateTimeField()
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+    User, on_delete=models.CASCADE, related_name="event_posts"
+    )
+    location = models.CharField(max_length=200)
+    description = models.TextField()
+    max_participants = models.PositiveIntegerField()
+    course_map = models.URLField(blank=True, null=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
--- myproject/settings.py
+++ myproject/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rbhhk88+3l+w%2q0+uxe62zyq_-67w=bmmn-xb*yn)!o_7%6m4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-jdohertydev-runit-jd1464g5zvp.ws-eu114.gitpod.io','.herokuapp.com']
 
@@ -88,6 +88,12 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.gitpod.io",
+    "https://*.codeanyapp.com",
+    "https://*.herokuapp.com"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
