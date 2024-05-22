dbd5e0e7d72109b6c15ab595decbe3e40cafa491
John Doherty <johndoherty1984@live.com>
2024-05-22 12:58:34 +0000

    Add methods to Comment model

--- events_listing/models.py
+++ events_listing/models.py
@@ -43,4 +43,10 @@ class Comment(models.Model):
         User, on_delete=models.CASCADE, related_name="commenter")
     body = models.TextField()
     approved = models.BooleanField(default=False)
-    created_on = models.DateTimeField(auto_now_add=True)    
+    created_on = models.DateTimeField(auto_now_add=True)   
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment '{self.body}' by {self.author}" 
