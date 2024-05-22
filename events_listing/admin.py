78f0d403f1e9ee312fe1ee63e2ec1af0bc62d75f
John Doherty <johndoherty1984@live.com>
2024-05-22 12:41:36 +0000

    Update admin.py wit comment model, migrate database

--- events_listing/admin.py
+++ events_listing/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import PostEvent
+from .models import PostEvent, Comment
 
 # Register your models here.
 admin.site.register(PostEvent)
+admin.site.register(Comment)
