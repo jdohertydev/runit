2066442940d32a9bb12a3bac304b0df2e0ad2117
John Doherty <johndoherty1984@live.com>
2024-05-23 15:27:37 +0000

    Add method to not show events that have passed

--- events_listing/views.py
+++ events_listing/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.views import generic
+from django.utils import timezone
 from .models import PostEvent
 
-# Create your views here.
-
 class PostList(generic.ListView):
-    queryset = PostEvent.objects.all()
     template_name = "events_listing/index.html"
     paginate_by = 6
+
+    def get_queryset(self):
+        # Get the current date and time
+        current_datetime = timezone.now()
+        
+        # Filter the queryset to include only events whose date is greater than or equal to the current date and time
+        queryset = PostEvent.objects.filter(date__gte=current_datetime)
+
+        return queryset
