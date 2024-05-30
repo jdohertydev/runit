1d675fef0e42a7de456998f747f2db66ecc440b5
John Doherty <johndoherty1984@live.com>
2024-05-30 12:26:23 +0000

    Update signup form so user has to add first and surname. Update postevent_details.html so participants list shows full name.

--- events_listing/forms.py
+++ events_listing/forms.py
@@ -1,4 +1,5 @@
 from .models import Comment
+from allauth.account.forms import SignupForm
 from django import forms
 
 
@@ -8,3 +9,14 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
+
+class CustomSignupForm(SignupForm):
+    first_name = forms.CharField(max_length=30, label='First Name', required=True)
+    last_name = forms.CharField(max_length=30, label='Last Name', required=True)
+
+    def save(self, request):
+        user = super(CustomSignupForm, self).save(request)
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.save()
+        return user
