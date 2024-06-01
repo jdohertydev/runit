bd64acd7ef81ee01ad38eddd5e3adb08cb27752c
John Doherty <johndoherty1984@live.com>
2024-06-01 11:44:49 +0000

    Update delete account function by adding modal

--- static/js/account_admin.js
+++ static/js/account_admin.js
@@ -1,7 +1,5 @@
 var togglePasswordChangeButton = document.getElementById('toggle-password-change');
-var toggleAccountDeleteButton = document.getElementById('toggle-account-delete');
 var passwordSection = document.getElementById('password-change-section');
-var accountDeleteSection = document.getElementById('account-delete-section');
 
 if (togglePasswordChangeButton) {
     togglePasswordChangeButton.addEventListener('click', function() {
@@ -9,16 +7,6 @@ if (togglePasswordChangeButton) {
             passwordSection.style.display = 'block';
         } else {
             passwordSection.style.display = 'none';
-        }
-    });
-}
-
-if (toggleAccountDeleteButton) {
-    toggleAccountDeleteButton.addEventListener('click', function() {
-        if (accountDeleteSection.style.display === 'none' || accountDeleteSection.style.display === '') {
-            accountDeleteSection.style.display = 'block';
-        } else {
-            accountDeleteSection.style.display = 'none';
         }
     });
 }
