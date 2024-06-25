e2b2aa9201f32676cb0a01428461e9b30e3f5b42
John Doherty <johndoherty1984@live.com>
2024-06-25 16:03:02 +0000

    Upload splash screen image

--- staticfiles/js/comments.js
+++ staticfiles/js/comments.js
@@ -13,7 +13,7 @@ const submitButton = document.getElementById("submitButton");
 
 for (let button of editButtons) {
   button.addEventListener("click", (e) => {
-    let commentId = e.target.getAttribute("comment_id");
+    let commentId = e.target.getAttribute("data-comment_id");
     let commentContent = document.getElementById(`comment${commentId}`).innerText;
     commentText.value = commentContent;
     submitButton.innerText = "Update";
@@ -27,7 +27,7 @@ const deleteConfirm = document.getElementById("deleteConfirm");
 
 for (let button of deleteButtons) {
   button.addEventListener("click", (e) => {
-    let commentId = e.target.getAttribute("comment_id");
+    let commentId = e.target.getAttribute("data-comment_id");
     deleteConfirm.href = `delete_comment/${commentId}`;
     deleteModal.show();
   });
